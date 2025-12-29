c725c5c00170f96aa980f4721317a0d6adbb5006
batchdelete <j5g2wmtzpv@privaterelay.appleid.com>
2025-12-28 21:09:32 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# macOS only check
 if [ "$(uname)" != "Darwin" ]; then
   echo "This script is intended for macOS only."
   exit 1
@@ -8,21 +7,21 @@ fi
 
 url="https://balls/oblivion.zip"
 
-TMP_DIR="/tmp/Oblivion"
-APP_NAME="Oblivion.app"
+TMP_DIR="/tmp/oblivion"
+APP_NAME="oblivion.app"
 APP_PATH="/Applications/$APP_NAME"
 
 mkdir -p "$TMP_DIR"
 
 if [ -d "$APP_PATH" ]; then
-  echo "Oblivion is already installed. Removing old version..."
+  echo "oblivion is already installed. Removing old version..."
   rm -rf "$APP_PATH"
 fi
 
-echo "Downloading Oblivion..."
+echo "Downloading oblivion..."
 curl -L -o "$TMP_DIR/oblivion.zip" "$url"
 
-echo "Extracting Oblivion..."
+echo "Extracting oblivion..."
 unzip -o "$TMP_DIR/oblivion.zip" -d "$TMP_DIR"
 
 echo "Installing to /Applications..."
@@ -30,4 +29,4 @@ mv -f "$TMP_DIR/$APP_NAME" "/Applications"
 
 rm -rf "$TMP_DIR"
 
-echo "Oblivion has been successfully installed!"
+echo "oblivion has been successfully installed!"
